678c46750cd4c966c31ec641dd6cc64c266171d7
Muddassirakhter <muddassirakhterr@gmail.com>
2024-09-14 14:17:50 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Ensure to add styling if necessary
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 Muddassir Akhter. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
